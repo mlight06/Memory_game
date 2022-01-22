738aae1d2f67c9e5e57edf21187ee6ad62a2222f
Michael Light <mklight06@gmail.com>
2022-01-22 10:10:07 -0700

    functionally rendering

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const PORT = 3100;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.json());
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
